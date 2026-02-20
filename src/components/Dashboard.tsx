cc276846f4093dd9943076f68b9f7c618ab14403
Mohit Tailor <Mohit@xcodethemes.in>
2026-02-20 18:30:44 +0530

    testing done

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Dashboard() {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser") || "{}");
+
+  const isAdmin = currentUser?.role === "admin";
+
+  return (
+    <div className="flex justify-center">
+      {isAdmin ? (
+        <div className="bg-amber-200 inline-block font-bold text-2xl mt-10 shadow ">
+          Welcome Back {' '}{currentUser.name}
+        </div>
+      ) : (
+        <div className="bg-amber-200 inline-block font-bold text-2xl mt-10 shadow ">
+          Welcome Back {' '} {currentUser.name}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dashboard;
